03d4ff8841c66bde3338b3f350ab62c4a7d2c2e4
vtsxking <rosas068@umn.edu>
2021-03-20 16:38:46 -0500

    switched dns nodes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -125,9 +125,8 @@ public:
         	143.198.77.205:7187
         	143.198.71.203:7187
         */
-        vSeeds.emplace_back("10.124.0.4");
-        vSeeds.emplace_back("10.124.0.3");
-        vSeeds.emplace_back("10.124.0.2");
+        vSeeds.emplace_back("20.83.170.92");
+        vSeeds.emplace_back("20.83.170.28");
         //vSeeds.emplace_back("dnsseed.ignitecoinpool.org");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,70); // 70 = V
