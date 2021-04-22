0a52240f41711739c19e1058c1df5bab94d8e444
vtsxking <kennyrosasmondragon@gmail.com>
2021-04-22 16:37:48 -0500

    missing semi-colon

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -60,7 +60,7 @@ static constexpr int64_t MINIMUM_CONNECT_TIME = 30;
 static constexpr uint64_t RANDOMIZER_ID_ADDRESS_RELAY = 0x3cac0035b5866b90ULL;
 /// Age after which a stale block will no longer be served if requested as
 /// protection against fingerprinting. Set to one month, denominated in seconds.
-static constexpr int STALE_RELAY_AGE_LIMIT = 3600 * 24 * 365 //30 * 24 * 60 * 60;
+static constexpr int STALE_RELAY_AGE_LIMIT = 3600 * 24 * 365; //30 * 24 * 60 * 60;
 /// Age after which a block is considered historical for purposes of rate
 /// limiting block relay. Set to one week, denominated in seconds.
 static constexpr int HISTORICAL_BLOCK_AGE = 7 * 24 * 60 * 60;
